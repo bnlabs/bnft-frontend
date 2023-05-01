c7589004d386c7c360d53cf0480b5cd31e121bef
Hy0tic <vothanhdat278@gmail.com>
2023-05-01 13:39:25 -0400

    logout button

--- src/components/BnftNavBar.js
+++ src/components/BnftNavBar.js
@@ -2,16 +2,55 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useSelector } from 'react-redux'; 
-import { useEffect } from 'react';
+import { useSelector,useDispatch } from 'react-redux'; 
+import { actions } from '../redux/userSlice.js';
 
 const BnftNavBar = () => {
+  const dispatch = useDispatch();
   const username = useSelector((state) => state.user.username);
+  const logout = () => {
+    dispatch(actions.setUser({}));
+    localStorage.clear();
+    console.log("logging out");
+  };
 
-  useEffect(() => {
-  }, [username]);
-  
-  return (
+  if(username){
+    return <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="home">Bnft</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="features">What is Bnft?</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="action">
+                  action
+                  </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+                <div>
+                <Nav.Link >
+                  Welcome back {username}
+                </Nav.Link>
+                <Nav.Link onClick={logout}>Logout</Nav.Link>
+                </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar></>
+  }
+  else{
+    return <>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="home">Bnft</Navbar.Brand>
@@ -34,13 +73,12 @@ const BnftNavBar = () => {
             </NavDropdown>
           </Nav>
           <Nav>
-            {username!==null ? <Nav.Link >Welcome back {username}</Nav.Link> : 
-              <Nav.Link href="https://discord.com/api/oauth2/authorize?client_id=1040068504826675251&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fredirect&response_type=code&scope=identify">Login With Discord</Nav.Link>} 
+              <Nav.Link href="https://discord.com/api/oauth2/authorize?client_id=1040068504826675251&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fredirect&response_type=code&scope=identify">Login With Discord</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
-  );
+    </Navbar></>
+  }
 }
 
 export default BnftNavBar;
