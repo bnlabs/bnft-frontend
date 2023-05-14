6c20f77df72caa47766458112fa4945682b67fb2
Hy0tic <vothanhdat278@gmail.com>
2023-05-13 21:06:15 -0400

    generate svg color based on info

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,13 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Button from './Button';
 import ResolutionSelection from './ResolutionSelection.js';
 import BnftLogo from "../BnftLogo";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import data from '../../colors.json';
+
+function getColor(index) {
+  // // Generate a random number between 0 and 16777215
+  // const randomColor = Math.floor(Math.random() * 16777215);
+
+  // // Convert the number to a 6-digit hex string
+  // const hexColor = randomColor.toString(16).padStart(6, '0');
+
+  // // Return the hex color string with a leading '#' character
+  // return `#${hexColor}`;
+
+  // console.log(data[index]['hex']);
+
+  return "#" + data[index]['hex'];
+}
 
 const SquareImage = () => {
   const [insideColor, setInsideColor] = useState("#2f81f7");
   const [outlineColor, setOutlineColor] = useState("#161b22");
+  const [bgColor, setBgColor] = useState("White");
+  const userId = useSelector((state) => state.user.id);
+
+  const lastSixDigitsId = userId.substring(userId.length - 6);
+
+  const bgIndex = parseInt(lastSixDigitsId.substring(0, 2)); // first two digits
+
+  const outlineIndex = parseInt(lastSixDigitsId.substring(2, 4)); // second and third digits
+
+  const fillColorIndex = parseInt(lastSixDigitsId.substring(4, 6)); // last two digits
 
   useEffect( ()=> {
 
@@ -16,14 +42,15 @@ const SquareImage = () => {
   const handleClick = () => {
     console.log("clicked"); 
 
-    setInsideColor("red");
-    setOutlineColor("black");
+    setBgColor(getColor(bgIndex));
+    setOutlineColor(getColor(outlineIndex));
+    setInsideColor(getColor(fillColorIndex));
   };
   
   return (
     <Container>
       <Square>
-        <BnftLogo length="40vh" color={insideColor} outlineColor={outlineColor}/>
+        <BnftLogo length="40vh" color={insideColor} outlineColor={outlineColor} bgColor={bgColor}/>
         <ResolutionSelection/>
         <Button func={() => handleClick()}/>
       </Square>
